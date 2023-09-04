cf04c9aefbe3e25f98c36da4906a491b756063be
shaneeKhant <shineshin222@gmail.com>
2023-09-04 20:57:43 +0630

    finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/*.{html,js}"],
+  darkMode: 'class',
   theme: {
+    fontFamily: {
+      'Roboto': ['Roboto Slab', 'serif'],
+      'Nunito Sans': ['Nunito Sans', 'sans - serif']
+    },
     extend: {},
   },
   plugins: [],
